02f86c03806f7d117f9a52fa286151285fc973a0
Ace <ace120387@gmail.com>
2017-11-12 12:13:13 +0000

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+
+
+module.exports={
+
+    entry:__dirname+'/src/app.js',
+    output:{
+        path:__dirname+'/public',
+        filename:'bundle.js'
+    },
+    module:{
+        rules:[{
+            loader:'babel-loader',
+            test:/\.js$/,
+            exclude:/node_modules/
+        }]
+    }
+};
